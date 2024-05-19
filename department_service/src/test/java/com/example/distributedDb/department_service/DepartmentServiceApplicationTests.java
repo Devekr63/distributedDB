1e323c287885a3b0b5dbd7f6cfbc6e74ed6e9fde
Devendra <devekr12@gmail.com>
2024-05-19 21:08:23 +0530

    project structure modified.

---
+++ department_service/src/test/java/com/example/distributedDb/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedDb.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
